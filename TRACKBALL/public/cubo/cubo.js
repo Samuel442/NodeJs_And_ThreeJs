34d1a2e6ca7a86f0faaa9102608aa54cb32d04d2
Samuel442 <59486233+Samuel442@users.noreply.github.com>
2023-02-28 18:36:43 -0300

    new functions

--- TRACKBALL/public/cubo/cubo.js
+++ TRACKBALL/public/cubo/cubo.js
@@ -1,70 +1,100 @@
 import { TrackballControls } from "./TrackballControls.js"; // importando a biblioteca GLTFLoader
 
-function init()
+var clock;
+function init_clock()
 {
+  clock = new THREE.Clock();   // armazena instantes de tempos da aplicação
+}
+             
+var scene;
+function init_scene()
+{
+  // construtor da cena
+  scene = new THREE.Scene();  // cria uma cena 
+}
+             
+var camera;
+function init_camera()
+{
+  // construtor da câmera
+camera = new THREE.PerspectiveCamera(75,  // parâmetros da câmera (75 = campo de visão,
+window.innerWidth / window.innerHeight,   // proporção da imagem dividindo a largura / altura,
+                                   0.1,   // 0.1 = plano de recorte mínimo 
+                                1000);    // 1000 = mais distantes máximo
+                                camera.position.z = 5;           // seta a posição da câmera em z = 5
+}
 
-var clock = new THREE.Clock();                // armazena instantes de tempos da aplicação
+var renderer;
+function init_renderer()
+{
+  // renderização
+  renderer = new THREE.WebGLRenderer();              // construtor da renderização criando um render
+  renderer.setSize(window.innerWidth, window.innerHeight); // seta a largura e altura
+  document.body.appendChild(renderer.domElement);          // adiciona um elemento dentro do corpo do documento html
 
-// construtor da cena
-const scene = new THREE.Scene();                // cria uma cena
+}
 
-// construtor da câmera
-const camera = new THREE.PerspectiveCamera(75,  // parâmetros da câmera (75 = campo de visão,
-      window.innerWidth / window.innerHeight,   // proporção da imagem dividindo a largura / altura,
-                                         0.1,   // 0.1 = plano de recorte mínimo 
-                                      1000);    // 1000 = mais distantes máximo
-
-
-// renderização
-const renderer = new THREE.WebGLRenderer();              // construtor da renderização criando um render
-renderer.setSize(window.innerWidth, window.innerHeight); // seta a largura e altura
-document.body.appendChild(renderer.domElement);          // adiciona um elemento dentro do corpo do documento html
-
-
+function add_cube()
+{
 // geometria do cubo
 const geometry = new THREE.BoxGeometry(2, 2, 2);          // construtor de geometria do cubo
-
-
 // material do cubo
-const material = new THREE.MeshBasicMaterial({color: 0x00ff00,wireframe: true,}); // cria um material será a textura
+const material = new THREE.MeshBasicMaterial({color: 0x00ff00,
+                                              wireframe: true,}); // cria um material será a textura
 const cube = new THREE.Mesh(geometry, material); // por fim é criado o cubo com a geometria e o material que é a textura
-scene.add(cube);                                 // adiciona o cubo na cena
+scene.add(cube);  // adiciona o cubo na cena
+}
+      
+function resize()
+{
+      // responsividade da tela
+      window.addEventListener('resize', onWindowResize, false);  // ouve eventos de alteração da dimensão da janela
+      function onWindowResize(){                                 // função que processa eventos de alterações da tela
+        camera.aspect = window.innerWidth / window.innerHeight;  // recebe a dimensão atual da janela
+        camera.updateProjectionMatrix();                         // atualia a projeção da janela
+        renderer.setSize(window.innerWidth, window.innerHeight); // altera as dimensoes da janela depois que alterou
+      }
+}
 
+var axes;
+function add_eixo()
+{
+  axes = new THREE.AxesHelper(20);
+  scene.add(axes);
 
-camera.position.z = 5;           // seta a posição da câmera em z = 5
+}
 
-    // responsividade da tela
-    window.addEventListener('resize', onWindowResize, false);  // ouve eventos de alteração da dimensão da janela
+var trackballControls;
+function add_trackballControls()
+{
+      // parte do trackball
+      trackballControls = new TrackballControls(camera,
+      renderer.domElement);
+      trackballControls.rotateSpeed = 1;
+      trackballControls.zoomSpeed = 1;
+      trackballControls.panSpeed = 1;
 
-    function onWindowResize(){                                 // função que processa eventos de alterações da tela
-      camera.aspect = window.innerWidth / window.innerHeight;  // recebe a dimensão atual da janela
-      camera.updateProjectionMatrix();                         // atualia a projeção da janela
-      renderer.setSize(window.innerWidth, window.innerHeight); // altera as dimensoes da janela depois que alterou
-    }
+}
 
-    var axes = new THREE.AxesHelper(20);
-    scene.add(axes);
-
-    // parte do trackball
-    const trackballControls = new TrackballControls(camera,
-                                       renderer.domElement);
-    trackballControls.rotateSpeed = 1;
-    trackballControls.zoomSpeed = 1;
-    trackballControls.panSpeed = 1;
-
-function animate() {              // cria uma função de animar a cena
+function render() {              // cria uma função de animar a cena
   var delta = clock.getDelta();   // difirença de tempo entre uma chamda da função animate e a próxima chamda da função animate
   trackballControls.update(delta); // calcula 
-  requestAnimationFrame(animate);  // recursivamente a função fica em loop
+  requestAnimationFrame(render);  // recursivamente a função fica em loop
   //cube1.rotation.x += 0.01;      // rotaciona o cubo em x nessa velocidade
   //cube1.rotation.y += 0.01;      // rotaciona o cubo em y nessa velocidade
   renderer.render(scene, camera); // renderiza a cena e a câmera exibindo o cubo
 }
 
-animate();                         // função é chamada
+init_clock()
+init_scene()
+init_camera()
+init_renderer()
+add_cube()
+resize()
+add_eixo()
+add_trackballControls()
+render()                   // função é chamada
 
-}
-window.onload = init;
 
 /* OBS: A função animate é chamada a cada vez que se desenha na tela, por exemplo se a aplicação tiver 
    60 frames por segundo, então a cada segundo a função animate é chamada 60 vezes, a variável delta
